8ff7dd29ae36de93348dbf9297ffebf1204cbab4
Musa <musabhat100@gmail.com>
2025-09-22 12:17:03 +0530

    Add files via upload

---
+++ LAB CODES/count_vowel_consonant.c
@@ -0,0 +1,26 @@
+// count_vowels_consonants.c
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+    int vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]); // convert to lowercase for easy checking
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+
+    return 0;
+}
