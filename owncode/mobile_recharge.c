9ff61cbfff79a6549a2863a3fc8e61c16aa3b77e
Musa <musabhat100@gmail.com>
2025-09-25 09:44:10 +0530

    Update mobile_recharge.c

--- owncode/mobile_recharge.c
+++ owncode/mobile_recharge.c
@@ -9,9 +9,10 @@ int main() {
     if (daysLeft > 3)
         printf("Recharge not urgent. You still have %d days left.\n", daysLeft);
     else if (daysLeft > 0)
-        printf("⚠️ Reminder: Recharge soon! Only %d days left.\n", daysLeft);
+        printf(" Reminder: Recharge soon! Only %d days left.\n", daysLeft);
     else
-        printf("❌ Your plan has expired. Please recharge immediately!\n");
+        printf(" Your plan has expired. Please recharge immediately!\n");
 
     return 0;
 }
+
